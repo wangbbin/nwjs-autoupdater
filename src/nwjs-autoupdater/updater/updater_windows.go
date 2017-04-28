ea34324ba53bad71e0172469fe06ab6654410c13
EvenAR <even.rognlien@gmail.com>
2017-04-28 11:35:10 +0200

    Windows updater: support for multiple subdirectories

--- src/nwjs-autoupdater/updater/updater_windows.go
+++ src/nwjs-autoupdater/updater/updater_windows.go
@@ -3,40 +3,55 @@ package updater
 import (
 	"os"
 	"path/filepath"
-
 	"github.com/mholt/archiver"
 )
 
 func Update(bundle, instDir, appName string) (error, string) {
 
-	// bundle: C:\Users\Even\AppData\Local\Temp\vnpcUpdate\update.zip
-	// instDir: C:\Users\Even\Documents\VNPCv2\nw_vnpc\build\VNPCv2\win32
-	// appName: VNPCv2
-
-
-///////////////////////////////////
 	extractDir := "./files";
 
 	appExecName := appName + ".exe"
-  appExec := filepath.Join(instDir, appExecName)
+  	appExec := filepath.Join(instDir, appExecName)
 
-  err := archiver.Zip.Open(bundle, extractDir)
+  	err := archiver.Zip.Open(bundle, extractDir)
 
 	err = filepath.Walk(extractDir, func(path string, f os.FileInfo, err error) error {
 			if(!f.IsDir()) {
-				newFile := path
-				fileToReplace := instDir + "\\" + f.Name()
-				fileToReplaceBackup := fileToReplace + ".bak"
+				extractedFile := path
+				relExtractDir, err := filepath.Rel(extractDir, path)	// remove "./files/" from path
+				instDirSubdir := filepath.Join(instDir, relExtractDir)	// installation sub-directory for the file
+				if err != nil {
+					return err
+				}
+				
+				oldFileToReplace := instDir + "\\" + relExtractDir
+				
+				
+				// Make sure the subdirectory/subdirectories (if any) for the new file exist 
+				if _, err = os.Stat(instDirSubdir); os.IsNotExist(err) {
+					os.MkdirAll(instDirSubdir, 0777)
+				}
 
-				err := os.Rename(fileToReplace, fileToReplaceBackup)
+				// If the extracted file exist in the installation, rename it to end with .bak
+				oldFileBackup := ""
+				if _, err = os.Stat(oldFileToReplace); err == nil {
+					oldFileBackup = oldFileToReplace + ".bak"
+					err = os.Rename(oldFileToReplace, oldFileBackup)
+					if err != nil {
+						return err
+					}
+				}
+
+				// Move the extracted file to instdir
+				err = os.Rename(extractedFile, oldFileToReplace)
 				if err != nil {
 					return err
 				}
-				err = os.Rename(newFile, fileToReplace)
-				if err != nil {
-					return err
+
+				// Remove the .bak-file
+				if oldFileBackup != "" {
+					os.Remove(oldFileBackup)
 				}
-				os.Remove(fileToReplaceBackup)
 			}
 			return nil
 	})
