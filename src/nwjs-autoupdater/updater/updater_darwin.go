6c7a68b750430118e69cff97c6c38c0a4e73fb31
Even <even@Evens-Mac.local>
2017-06-21 23:24:29 +0200

    Electron support

--- src/nwjs-autoupdater/updater/updater_darwin.go
+++ src/nwjs-autoupdater/updater/updater_darwin.go
@@ -1,71 +1,88 @@
 package updater
 
 import (
-  "io/ioutil"
   "os"
   "path/filepath"
-
-  xattr "github.com/ivaxer/go-xattr"
   "github.com/mholt/archiver"
-
   "github.com/skratchdot/open-golang/open"
 )
 
-// Update ...
 func Update(bundle, instDir, appName string) (error, string) {
+
+  extractDir := "./files";
+  if _, err := os.Stat(extractDir); err == nil {
+    os.RemoveAll(extractDir)
+  }
+
   appExecName := appName + ".app"
-  appExec := filepath.Join(instDir, appExecName)
-  appDir := appExec
-  appBak := appExec + ".bak"
+    appExec := filepath.Join(instDir, appExecName)
 
-  tempDir, err := ioutil.TempDir("", appName)
+    err := archiver.Zip.Open(bundle, extractDir)
+
+  err = filepath.Walk(extractDir, func(path string, f os.FileInfo, err error) error {
+    if(!f.IsDir()) {
+      extractedFile := path
+
+      // Remove "./files/" from path
+      relExtractPath, err := filepath.Rel(extractDir, extractedFile)  
+
+      // Remove filename from path
+      relExtractDir := filepath.Dir(relExtractPath) 
+
+      // Installation sub-directory for the file      
+      instDirSubdir := filepath.Join(instDir, relExtractDir)      
+      if err != nil {
+        return err
+      }
+      
+      // Full installation path of the new file
+      newFileInstPath := filepath.Join(instDir, relExtractPath)   
+
+      // Make sure the subdirectory/subdirectories (if any) for the new file exist 
+      if _, err = os.Stat(instDirSubdir); os.IsNotExist(err) {
+        os.MkdirAll(instDirSubdir, 0777)
+      }
+
+      // If the extracted file exist in the installation, rename it to end with .bak
+      oldFileBackup := ""
+      if _, err = os.Stat(newFileInstPath); err == nil {
+        oldFileBackup = newFileInstPath + ".bak"
+        err = os.Rename(newFileInstPath, oldFileBackup)
+        if err != nil {
+          return err
+        }
+      }
+
+      // Move the extracted file to instdir
+      err = os.Rename(extractedFile, newFileInstPath)
+      if err != nil {
+        return err
+      }
+
+      // Remove the .bak-file
+      if oldFileBackup != "" {
+        os.Remove(oldFileBackup)
+      }
+    }
+    return nil
+  })
+
+  err = os.RemoveAll(extractDir)
   if err != nil {
     return err, appExec
   }
-  defer os.RemoveAll(tempDir)
-
-  err = archiver.Zip.Open(bundle, tempDir)
-  if err != nil {
-    return err, appExec
-  }
-
-  err = os.Rename(appDir, appBak)
-  if err != nil {
-    return err, appExec
-  }
-
-  updateFiles := filepath.Join(tempDir, appExecName)
-
-  err = os.Rename(updateFiles, appExec)
-  if err != nil {
-    os.RemoveAll(appExec)
-    os.Rename(appBak, appExec)
-
-    return err, appExec
-  }
-
-  err = os.RemoveAll(appBak)
-  if err != nil {
-    return err, appExec
-  }
-
   err = os.RemoveAll(bundle)
   if err != nil {
     return err, appExec
   }
 
-  attrData, _ := xattr.Get(appExec, "com.apple.quarantine")
-
-  if len(attrData) == 0 {
-    return nil, appExec
-  }
-
-  err = xattr.Remove(appExec, "com.apple.quarantine")
+  /*cmd := exec.Command(appExec)
+  err = cmd.Start()
   if err != nil {
     return err, appExec
-  }
+  }*/
 
   open.Start(appExec)
-
+  
   return nil, appExec
 }
